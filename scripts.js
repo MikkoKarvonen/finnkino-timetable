6653bb0d228dc145b70e17a7f69624c91ebfb05d
DJMiigii <mkarvonen34@gmail.com>
2017-11-23 12:41:48 +0200

    Make site mobile friendly

--- scripts.js
+++ scripts.js
@@ -147,3 +147,19 @@ function showTime() {
   }, 60000);
 }
 showTime();
+
+document.addEventListener('DOMContentLoaded', function () {
+  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  if ($navbarBurgers.length > 0) {
+    $navbarBurgers.forEach(function ($el) {
+      $el.addEventListener('click', function () {
+        var target = $el.dataset.target;
+        var $target = document.getElementById(target);
+
+        $el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+      });
+    });
+  }
+});
