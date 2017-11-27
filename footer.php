6d3b159ffd70c2878b97fd5a44b67a74e3de43d4
DJMiigii <mkarvonen34@gmail.com>
2017-11-27 13:32:36 +0200

    Add info

--- footer.php
+++ footer.php
@@ -1,3 +1,13 @@
+    <section class="hero">
+    <div class="hero-body">
+    <div class="container has-text-centered">
+      <h2 class="subtitle">
+        By Mikko Karvonen, <?php echo date('Y')?>
+      </h2>
+      <a class="button is-small is-info" href="https://github.com/MikkoKarvonen/finnkino-timetable" target="_blank">Code</a>
+    </div>
+    </div>
+    </section>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="scripts.js"></script>
   </body>
